a055131e0b7bf5d323eadb3f4af5135d04e07127
Arun John <arjkizhakkedathu@gmail.com>
2022-04-08 17:11:04 +0530

    Revert "[Added]: Wsywyg Editior"
    This reverts commit 668b6cf5de6359291c9bee2165f96c6a5e9e72b1.

--- config/plugins.js
+++ config/plugins.js
@@ -2,21 +2,16 @@ module.exports = ({
   env
 }) => ({
   upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
-        },
-      },
-    },
-  },
-  wysiwyg: {
-    enabled: true,
-    resolve: './src/plugins/wysiwyg'
-  },
-
+     config: {
+       provider: 'aws-s3',
+       providerOptions: {
+         accessKeyId: env('AWS_ACCESS_KEY_ID'),
+         secretAccessKey: env('AWS_ACCESS_SECRET'),
+         region: env('AWS_REGION'),
+         params: {
+           Bucket: env('AWS_BUCKET_NAME'),
+         },
+       },
+     },
+   },
 });
