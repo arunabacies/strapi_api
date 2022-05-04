14b08dc66a8b8275132e11ca5bb5272dd332b81e
Arun John <arjkizhakkedathu@gmail.com>
2022-05-04 09:38:12 +0530

    [Update]: JWT Token Expiry added

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,21 @@
-module.exports = ({ env }) => ({
+module.exports = ({
+  env
+}) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'demo_token_salt'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'd19cd215f26ea1c35d9f655536e27a53'),
+    events: {
+      onConnectionSuccess(e) {
+        console.log(e.user, e.provider);
+      },
+      onConnectionError(e) {
+        console.error(e.error, e.provider);
+      },
+    },
+    options: {
+      expiresIn: "30d",
+    }
   },
 });
