9912bd02b027c20b321c8ca2fc4811521f0d1109
Arun John <arjkizhakkedathu@gmail.com>
2022-05-26 13:45:02 +0530

    [Update]

--- config/database.js
+++ config/database.js
@@ -11,39 +11,39 @@ const path = require('path');
 // });
 
 // Uncomment to Activate PostgreSQL DB
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// module.exports = ({
-//   env
-// }) => ({
-//   connection: {
-//     client: 'postgres',
-//     host: config.host,
-//     port: config.port,
-//     database: config.database,
-//     user: config.user,
-//     password: config.password,
-//     ssl: {
-//       rejectUnauthorized: false
-//     },
-//     debug: false,
-//   },
-// });
-
-
-// Uncomment to Activate MySQL DB
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({
   env
 }) => ({
   connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "default"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
+    client: 'postgres',
+    host: config.host,
+    port: config.port,
+    database: config.database,
+    user: config.user,
+    password: config.password,
+    ssl: {
+      rejectUnauthorized: false
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
+
+
+// Uncomment to Activate MySQL DB
+// module.exports = ({
+//   env
+// }) => ({
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env("DATABASE_PORT", 3306),
+//       database: env("DATABASE_NAME", "default"),
+//       user: env("DATABASE_USERNAME", "root"),
+//       password: env("DATABASE_PASSWORD", ""),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
